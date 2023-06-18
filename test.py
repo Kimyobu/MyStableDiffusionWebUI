a8a46d684177eba9dde68db1455658d2c7da2e95
Kimyobu <phanarin677@gmail.com>
2023-06-18 18:11:04 +0700

    Add dropdown model import select

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import datetime
- 
-today = datetime.datetime.today()
-print(f"{today}")
+model = 'Stable Diffusion' #@param ["Lora", "Stable Diffusion"]
+model_list = {'Lora': 'Lora', 'Stable Diffusion': 'Stable-diffusion'}
+model_path = '/content/drive/MyDrive/stable-diffusion-webui-colab/alohomora/models/' + model_list.get(model)  # type: ignore
+print(model_path)
